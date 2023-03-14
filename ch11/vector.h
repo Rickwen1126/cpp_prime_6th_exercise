e366d8f357f5a1dc6eeba891ffbf90e6311ca496
RickWen <rickwen1126@gmail.com>
2023-03-14 11:51:50 +0800

    Add vector for ch11

--- ch11/vector.h
+++ ch11/vector.h
@@ -1,5 +1,6 @@
 #ifndef VECTOR_H
 #define VECTOR_H
+#include <iostream>
 
 namespace VECTOR {
     class Vector {
@@ -15,21 +16,29 @@ namespace VECTOR {
             void set_ang();
             void set_x();
             void set_y();
+            void set_val(double n1, double n2, Mode form);
         public:
             Vector();
             Vector(double n1, double n2, Mode form = RECT);
             void reset(double n1, double n2, Mode form = RECT);
             ~Vector();
-            double xval() const {return x;}
-            double yval() const {return y;}
-            double magval() const {return mag;}
-            double angval() const {return ang;}
+            double xval() const {return x;};
+            double yval() const {return y;};
+            double magval() const {return mag;};
+            double angval() const {return ang;};
             void polar_mode();
             void rect_mode();
             Vector operator+(const Vector &b) const;
             Vector operator-(const Vector &b) const;
             Vector operator-() const;
-            
+            Vector operator*(double n) const;
+
+            // friend functions
+            friend Vector operator*(double n , const Vector &a);
+            friend std::ostream& operator<<(std::ostream &os, const Vector &v);
+
+            // conversion functions
+            operator double();
     };
 }
 
