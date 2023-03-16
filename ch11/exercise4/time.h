1793f88772a3e40c137f6238a9e298ec7e8084ee
Rick Wen <rickwen1126@gmail.com>
2023-03-16 13:53:57 +0800

    ch11 ex4 in processing

---
+++ ch11/exercise4/time.h
@@ -0,0 +1,27 @@
+#ifndef MYTIME_H
+#define MYTIME_H
+
+#include <iostream>
+
+class Time {
+    Time();
+    Time(int h = 0, int m = 0);
+    void AddMin(int m);
+    void AddHr(int h);
+    void Reset(int h = 0, int m = 0);
+    Time operator+(const Time &t) const;
+    Time operator-(const Time &t) const;
+    Time operator*(double n) const;
+
+    friend Time operator+(const Time &t1, const Time &t2);
+    friend Time operator-(const Time &t1, const Time &t2);
+    friend Time operator*(const Time &t1, const Time &t2);
+
+    friend Time operator*(double m, const Time &t)
+    {
+        return t * m;
+    } // inline definition
+    friend std::ostream &operator<<(std::ostream &os, const Time &t);
+};
+
+#endif
