9749b11f4dd99f5df8a93f1ecdaabfa974510183
RickWen <rickwen1126@gmail.com>
2023-03-17 17:58:06 +0800

    ch11 ex5 in processing

--- ch11/exercise5/stonewt.cpp
+++ ch11/exercise5/stonewt.cpp
@@ -2,16 +2,18 @@
 #include <iostream>
 
 
-Stonewt::Stonewt(double lbs) {
+Stonewt::Stonewt(double lbs, Mode form) {
     stone = int (lbs) / Lbs_per_stn;
     pds_left = int (lbs) % Lbs_per_stn + lbs - int(lbs);
     pounds = lbs;
+    mode = form;
 }
 
-Stonewt::Stonewt(int stn, double lbs) {
+Stonewt::Stonewt(int stn, double lbs, Mode form) {
     stone = stn;
     pds_left = lbs;
     pounds = stn * Lbs_per_stn + lbs;
+    mode = form;
 }
 
 Stonewt::Stonewt() {
@@ -22,8 +24,51 @@ Stonewt::~Stonewt() {
 
 }
 
-void Stonewt::show_stn() const {
-    std::cout << stone << " stone, " << pds_left << " pounds\n";
+// void Stonewt::show_stn() const {
+//     switch (mode) {
+//         case MODE_ST:
+//             std::cout << stone << " stone, " << pds_left << " pounds\n";        
+//             break;
+//     }
+//     std::cout << stone << " stone, " << pds_left << " pounds\n";
+// }
+
+// void Stonewt::show_lbs() const {
+//     std::cout << pounds << " pounds\n";
+// }
+
+std::ostream & operator<<(std::ostream &os, const Stonewt &st) {
+    switch (st.mode) {
+        case Stonewt::MODE_ST:
+            os << st.stone << " stone\n";
+            break;
+        case Stonewt::MODE_INT:
+            os << (int)st.pounds + 0.5 << " pounds\n";
+            break;
+        case Stonewt::MODE_FLOAT:
+            os << st.pounds << " pounds\n";
+            break;
+    }
+}
+
+Stonewt Stonewt::operator+(const Stonewt &other) const {
+    return Stonewt(pounds + other.pounds);
+}
+
+Stonewt Stonewt::operator-(const Stonewt &other) const {
+    return Stonewt(pounds - other.pounds);
+}
+
+Stonewt Stonewt::operator*(const Stonewt &other) const {
+    return Stonewt(pounds * other.pounds);
+}
+
+Stonewt::operator int() const {
+    return int (pounds + 0.5);
+}
+
+Stonewt::operator double() const {
+    return pounds;
 }
 
 Stonewt Stonewt::operator*(double val) const {
@@ -39,4 +84,3 @@ Stonewt operator*(double mult, const Stonewt &s2) {
     return Stonewt(mult * s2.pounds);
 }
 
-// void Stonewt::
--- ch11/exercise5/stonewt.h
+++ ch11/exercise5/stonewt.h
@@ -1,26 +1,30 @@
 #ifndef STONEWT_H
 #define STONEWT_H
+#include <iostream>
 
 class Stonewt {
     private: 
         enum {Lbs_per_stn = 14};
-        enum Mode{MODE_INT, MODE_FLOAT};
+        enum Mode{MODE_ST, MODE_INT, MODE_FLOAT};
         int stone;
         double pds_left;
         double pounds;
+        Mode mode;
     public:
-        Stonewt(double lbs);
-        Stonewt(int stn, double lbs);
+        Stonewt(double lbs, Mode form = MODE_ST);
+        Stonewt(int stn, double lbs, Mode form = MODE_ST);
         Stonewt();
         ~Stonewt();
-        void show_lbs() const;
-        void show_stn() const;
+        Stonewt operator+(const Stonewt &other) const;
+        Stonewt operator-(const Stonewt &other) const;
+        Stonewt operator*(const Stonewt &other) const;
         Stonewt operator*(double val) const;
         // conversion function
         operator int() const;
         operator double() const;
         friend Stonewt operator*(const Stonewt &s1, const Stonewt &s2);
         friend Stonewt operator*(double mult, const Stonewt &s2);
+        friend std::ostream &operator<<(std::ostream &os, const Stonewt &st);
 };
 
 #endif
